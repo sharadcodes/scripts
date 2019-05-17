8192d56c6d38ab733bca31b5419f47221ac59403
SHARAD RAJ <36638057+sharadcodes@users.noreply.github.com>
2019-05-17 18:49:17 +0530

    Create readme_color.sh

---
+++ github/readme_color.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+FILE='colors.md'
+
+if [ -f $FILE ]; then
+   echo "File $FILE exists."
+else
+	touch colors.md
+	chmod +x colors.md
+fi
+
+read -p 'color code: ' input_color
+
+echo "- ![#"$input_color"](https://placehold.it/15/"$input_color"/000000?text=+) "#$input_color"" >> colors.md
