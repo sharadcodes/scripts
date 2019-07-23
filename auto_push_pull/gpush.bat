ed88619582eaf9a1628340d05b727b508e6f1737
SHARAD RAJ <36638057+sharadcodes@users.noreply.github.com>
2019-07-23 17:39:54 +0530

    Update gpush.bat

--- auto_push_pull/gpush.bat
+++ auto_push_pull/gpush.bat
@@ -1,14 +1,14 @@
 @echo off
 set /p message="Enter message : "
 echo.
-echo "+------ ADDING FILES      ------+"
+echo ------- ADDING FILES      -------
 echo.
 git add .
 echo.
-echo "+------ MAKING COMMIT     ------+"
+echo ------- MAKING COMMIT     -------
 echo.
 git commit -m "%message%"
 echo.
-echo "+------ PUSHING TO GITHUB ------+"
+echo ------- PUSHING TO GITHUB -------
 echo.
 git push
